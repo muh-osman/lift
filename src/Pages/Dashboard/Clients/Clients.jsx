ed3ad405b8ccebc127cba6f7f37e9b09c34af42f
muh-osman <mr.muhammed.osman@gmail.com>
2024-09-02 13:03:56 +0300

    Fix

--- src/Pages/Dashboard/Clients/Clients.jsx
+++ src/Pages/Dashboard/Clients/Clients.jsx
@@ -14,34 +14,91 @@ import { useDeleteClientApi } from "../../../API/useDeleteClientApi";
 import { toast } from "react-toastify";
 
 const columns = [
-  { field: "id", headerName: "ID", flex: 1, minWidth: 50 },
-  { field: "name", headerName: "اسم العميل", flex: 1, minWidth: 175 },
-  { field: "phone_number", headerName: "رقم الهاتف", flex: 1, minWidth: 110 },
-  { field: "neighborhood", headerName: "الحي", flex: 1, minWidth: 100 },
-  { field: "maintenance_type", headerName: "الصيانة", flex: 1, minWidth: 100 },
-  { field: "spare_parts", headerName: "قطع الغيار", flex: 1, minWidth: 100 },
-  { field: "service_type", headerName: "نوع الخدمة", flex: 1, minWidth: 100 },
+  { field: "id", headerName: "ID", flex: 1, minWidth: 50, sortable: false },
+  {
+    field: "name",
+    headerName: "اسم العميل",
+    flex: 1,
+    minWidth: 175,
+    sortable: false,
+  },
+  {
+    field: "phone_number",
+    headerName: "رقم الهاتف",
+    flex: 1,
+    minWidth: 110,
+    sortable: false,
+  },
+  {
+    field: "neighborhood",
+    headerName: "الحي",
+    flex: 1,
+    minWidth: 100,
+    sortable: false,
+  },
+  {
+    field: "maintenance_type",
+    headerName: "الصيانة",
+    flex: 1,
+    minWidth: 100,
+    sortable: false,
+  },
+  {
+    field: "spare_parts",
+    headerName: "قطع الغيار",
+    flex: 1,
+    minWidth: 100,
+    sortable: false,
+  },
+  {
+    field: "service_type",
+    headerName: "نوع الخدمة",
+    flex: 1,
+    minWidth: 100,
+    sortable: false,
+  },
   {
     field: "contract_start_date",
     headerName: "بداية العقد",
     flex: 1,
     minWidth: 125,
+    sortable: false,
   },
   {
     field: "contract_end_date",
     headerName: "نهاية العقد",
     flex: 1,
     minWidth: 125,
+    sortable: false,
   },
   {
     field: "maintenance_value",
     headerName: "قيمة الصيانة",
     flex: 1,
     minWidth: 100,
+    sortable: false,
   },
-  { field: "paid", headerName: "مدفوع", flex: 1, minWidth: 100 },
-  { field: "unpaid", headerName: "غير مدفوع", flex: 1, minWidth: 100 },
-  { field: "notes", headerName: "ملاحظات", flex: 1, minWidth: 275 },
+  {
+    field: "paid",
+    headerName: "مدفوع",
+    flex: 1,
+    minWidth: 100,
+    sortable: false,
+  },
+  {
+    field: "unpaid",
+    headerName: "غير مدفوع",
+    flex: 1,
+    minWidth: 100,
+    sortable: false,
+  },
+  {
+    field: "notes",
+    headerName: "ملاحظات",
+    flex: 1,
+    minWidth: 275,
+    sortable: false,
+  },
 ];
 
 export default function Clients() {
@@ -106,6 +163,32 @@ export default function Clients() {
       notes: client.notes,
     })) || [];
 
+  //
+  const [containerWidth, setContainerWidth] = useState(
+    window.innerWidth < 600 ? window.innerWidth - 48 : "100%"
+  );
+
+  const updateContainerWidth = () => {
+    if (window.innerWidth < 600) {
+      setContainerWidth(window.innerWidth - 48);
+    } else {
+      setContainerWidth("100%");
+    }
+  };
+
+  useEffect(() => {
+    // Set initial width
+    updateContainerWidth();
+
+    // Update width on window resize
+    window.addEventListener("resize", updateContainerWidth);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", updateContainerWidth);
+    };
+  }, []);
+
   return (
     <div className={style.container}>
       {fetchStatus === "fetching" && (
@@ -158,10 +241,9 @@ export default function Clients() {
       </Stack>
 
       <div
+        className={style.datagrid_container}
         style={{
-          height: "calc(100vh - 178px)",
-          width: "100%",
-          overflow: "auto",
+          width: containerWidth, // Set width dynamically
         }}
       >
         <DataGrid
@@ -175,6 +257,10 @@ export default function Clients() {
           pageSizeOptions={[10, 25, 50, 100]}
           checkboxSelection
           disableMultipleRowSelection
+          disableColumnFilter // Disable filtering
+          disableColumnSort // Disable sorting
+          disableMultipleColumnSorting // Disable multiple column sorting
+          disableColumnMenu // Hide column menu
           onRowSelectionModelChange={handleSelectionChange}
           style={{ width: "100%", height: "100%", overflowX: "auto" }}
         />
